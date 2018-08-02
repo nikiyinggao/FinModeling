f694b94082ce3aee65e42387eaa760ec0873298a
niki gao <33434986+nikiyinggao@users.noreply.github.com>
2018-08-01 22:05:39 -0400

    Add files via upload

---
+++ GARCH_predict.R
@@ -0,0 +1,228 @@
+
+
+path <- "/Users/niki/Desktop/Modeling&Eng/GARCH_MODEL/Data.csv"
+data.file <- read.csv(path,header=TRUE)
+
+str(data.file)
+head(data.file)
+tail(data.file)
+
+# Create data frame object
+SPX.df.date <- as.Date(data.file$SPX_DATE, format="%m/%d/%Y")
+SPX.data.frame <- cbind(SPX.df.date, data.file[,5:6])
+SPX.data.frame <- na.omit(SPX.data.frame[,1:3])
+head(SPX.data.frame)
+tail(SPX.data.frame)
+
+# Create time series object
+SPX.time.series <- xts(SPX.data.frame[,2:3], order.by=SPX.data.frame[,1])
+head(SPX.time.series)
+tail(SPX.time.series)
+
+# Calculate log returns and get rid of first observation
+SPX.log.returns <- diff(log(SPX.time.series$SPX_CLOSE))
+SPX.log.returns <- SPX.log.returns[-1,]
+
+# Plot time series
+class(SPX.log.returns)
+SPX.log.returns.df <-cbind(index(SPX.log.returns),data.frame(SPX.log.returns))
+names(SPX.log.returns.df) <- paste(c("date","spx"))
+head(SPX.log.returns.df)
+class(SPX.log.returns.df)
+
+meanlevel <- mean(SPX.log.returns.df$spx)
+sdlevel <- sd(SPX.log.returns.df$spx)
+
+plot(SPX.log.returns.df$date, SPX.log.returns.df$spx, axes=TRUE, ylab = "Log Returns",
+     xlab="",ylim=c(-.15,.15), col = ifelse(abs(SPX.log.returns.df$spx) > 2*sdlevel,'red','black'),cex.axis = .7)
+title(main="SPX Log Returns", cex=.7)
+mtext("Date",side=1,col="black",line=2)
+text(x=as.Date('2010-10-15'), y=.135, labels='Volatility Clustering', col="black", cex=.75)
+
+arrows(x0=as.Date('2010-10-15'), y0=.13, x1=as.Date('2009-05-01'), y1=.07, col='black', length=0.1, lwd=1)
+arrows(x0=as.Date('2010-10-15'), y0=.13, x1=as.Date('2011-07-01'), y1=.07, col='black', length=0.1, lwd=1)
+text(x=as.Date('2010-10-15'), y=-.135, labels='Volatility Clustering', col="black", cex=.75)
+
+arrows(x0=as.Date('2010-10-15'), y0=-.13, x1=as.Date('2009-05-01'), y1=-.07, col='black', length=0.1, lwd=1)
+arrows(x0=as.Date('2010-10-15'), y0=-.13, x1=as.Date('2011-07-01'), y1=-.07, col='black', length=0.1, lwd=1)
+abline(h=meanlevel + 2 * sdlevel, col = "red", lty=2, lwd=2)
+abline(h=meanlevel - 2 * sdlevel, col = "red", lty=2, lwd=2)
+abline(h=meanlevel + 3 * sdlevel, col = "blue", lty=2, lwd=2)
+abline(h=meanlevel - 3 * sdlevel, col = "blue", lty=2, lwd=2)
+legend("topleft",legend=c("+/-2 Standard Deviations","+/- 3 Standard Deviations"),bty="n",lty=c(2,2), cex=0.80, col=c("red","blue")) 
+
+#equity index log returns
+SPX.obs <- length(SPX.log.returns)
+SPX.min.return <- min(SPX.log.returns)
+SPX.mean.return <- mean(SPX.log.returns)
+SPX.median.return <- median(SPX.log.returns)
+SPX.max.return <- max(SPX.log.returns)
+SPX.skew.return <- skewness(SPX.log.returns)
+SPX.kurt.return <- kurtosis(SPX.log.returns)
+SPX.ann.sd <- sd(SPX.log.returns)*sqrt(252)
+SPX.JB.stat <- jarque.bera.test(SPX.log.returns)$statistic
+SPX.JB.pvalue <- jarque.bera.test(SPX.log.returns)$p.value
+
+# Matrix of outputs
+Summary.stats <- as.data.frame(matrix(data=NA,nrow = 10, ncol = 1))
+
+Summary.stats[1,1] <- SPX.obs
+Summary.stats[2,1] <- SPX.min.return
+Summary.stats[3,1] <- SPX.mean.return
+Summary.stats[4,1] <- SPX.median.return
+Summary.stats[5,1] <- SPX.max.return
+Summary.stats[6,1] <- SPX.skew.return
+Summary.stats[7,1] <- SPX.kurt.return
+Summary.stats[8,1] <- SPX.ann.sd
+Summary.stats[9,1] <- SPX.JB.stat
+Summary.stats[10,1] <- SPX.JB.pvalue
+
+rownames(Summary.stats) <- c("No. Obs.", "Min", "Mean", "Median", "Max", "Skewness",
+                                  "Kurtosis", "Ann. Std.", "Jarque-Bera Statistic", "Jarque-Bera p-Value")
+colnames(Summary.stats) <- c("SPX")
+Summary.stats
+
+# Start and end dates 
+SPX.start.date <- as.Date(SPX.data.frame[1,1],format="%m/%d/%Y")
+SPX.end.date <- as.Date(SPX.data.frame[SPX.obs+1,1],format="%m/%d/%Y")
+Summary.dates <- data.frame(c(SPX.start.date, SPX.end.date))
+Summary.dates
+
+# Histogram of log returns to index and Normal and Student-t distributions superimposed
+hist_returns <- SPX.log.returns
+hist_mean <- mean(hist_returns)
+hist_std <- sqrt(var(hist_returns))
+
+hist.SPX <- hist(hist_returns, breaks=100)
+plot(hist.SPX, xlim=c(-.05,.05), ylim=c(0,60), freq=F, xlab="Log Return",col="white",
+     main="Histogram of SPX Log Returns, Normal and Student-t Densities (dof=3)", cex.main=.85, cex.axis=.70, cex.lab=.75)
+
+# 3 degrees of freedom 
+curve(dt(x*sqrt(3)/hist_std,df=3)*sqrt(3)/hist_std,from=-.05,to=.05,col="red", add=TRUE, lty=2,lwd=2, yaxt="n")
+curve(dnorm(x, mean=hist_mean, sd=hist_std), from=-.05,to=.05,col="blue", add=TRUE, lty=2,lwd=2, yaxt="n")
+legend("topleft",legend=c("Normal Density","Student-t Density (dof=3)"),bty="n", lty=c(2,2), cex=0.80, col=c("blue","red")) 
+
+# QQ Plots 
+qqPlot(coredata(SPX.log.returns),distribution = "norm", main = "QQ Plot for SPX Log Returns - Normal Distribution", 
+       xlab="Normal Quantiles", ylab="Log Return Quantiles", cex=.5, envelope = .95, grid=FALSE)
+qqPlot(coredata(SPX.log.returns),distribution = "t", df=3,main = "QQ Plot for SPX Log Returns - Student-t Distribution (dof=3)",
+       xlab="Student-t Quantiles",ylab="Log Return Quantiles", cex=.5, envelope = .95, grid=FALSE)
+
+
+# ACFs and squared log returns
+par(mfrow=c(2,1))
+par(mar=c(4,4,3,4))
+acf(SPX.log.returns,lag.max=20, xlab = "Lag", main="SPX Log Returns", ci.col="blue",lty=3,lwd=4,col="red")
+mtext(side=3,'(Weak to no autocorrelation \n in the ACF of SPX log-returns)', col="black", cex=.75)
+acf(SPX.log.returns^2,lag.max=20, xlab = "Lag", main="SPX Log Returns Squared", ci.col="blue",lty=3,lwd=4,col="red")
+mtext(side=3,'(Clear pattern of autocorrelation \n in the ACF of SPX log-returns squared)', col="black", cex=.75)
+
+# rolling annualized historical volatility
+# get tabular statistics for one-month rolling volatility values
+# n.roll.real.vol is used for number of trading days to calculate the rolling realized annualized monthly volatility and 
+
+# n is used to subset the time series to drop 
+n.roll.real.vol <- 22
+
+Roll.real.vol <- function(data,n){
+  
+  vol <- data[n:length(data)]
+  
+  for(i in n:length(data)) {
+    
+    # Use simple sum of squared returns with mean assumed equal to zero 
+    #Making the realized volatility comparable to the implied volatility index values
+    vol[(i-n)+1] <- sqrt(sum(data[((i-n)+1):i]^2)/(n))*sqrt(252)
+    
+  }
+  
+  vol
+  
+}
+
+# Calculate rolling realized volatility for log returns
+SPX.rolling.vol <- Roll.real.vol(SPX.log.returns.df$spx,n.roll.real.vol)
+
+# Summary statistics for rolling actual realized one-month volatility
+SPX.real.vol.obs <- length(SPX.rolling.vol)
+SPX.real.vol.min <- min(SPX.rolling.vol)
+SPX.real.vol.mean <- mean(SPX.rolling.vol)
+SPX.real.vol.median <- median(SPX.rolling.vol)
+SPX.real.vol.max <- max(SPX.rolling.vol)
+
+# Matrix of outputs
+Summary.real.vol.stats <- as.data.frame(matrix(data=NA,nrow = 5, ncol = 1))
+
+Summary.real.vol.stats[1,1] <- SPX.real.vol.obs
+Summary.real.vol.stats[2,1] <- SPX.real.vol.min
+Summary.real.vol.stats[3,1] <- SPX.real.vol.mean
+Summary.real.vol.stats[4,1] <- SPX.real.vol.median
+Summary.real.vol.stats[5,1] <- SPX.real.vol.max
+
+rownames(Summary.real.vol.stats) <- c("No. Obs.","Min", "Mean", "Median", "Max")
+colnames(Summary.real.vol.stats) <- c("SPX")
+Summary.real.vol.stats
+
+
+# GARCH modeling
+
+SPX.data <- SPX.log.returns.df$spx*100
+n.GARCH.start <- 1260
+n.GARCH.end <- 22
+
+# use in loop 
+GARCH.spec.norm <- ugarchspec(mean.model=list(armaOrder=c(0,0)),
+                        variance.model=list(garchOrder=c(1,1)),distribution="norm") 
+
+# Rolling historical volatility estimator for GARCH(1,1) with normal innovations
+Roll.GARCH.norm.est <- function(data,n.GARCH.start,n.GARCH.end){
+  
+  nlast <- length(data)-n.GARCH.end
+  GARCH.est <- data[n.GARCH.start:nlast]
+  for(i in n.GARCH.start:(length(data)-n.GARCH.end)) {
+    
+    print(i)
+    
+    data.set <- data[((i-n.GARCH.start)+1):i]
+ 
+    GARCH.fit <- ugarchfit(data=data.set,spec=GARCH.spec.norm, solver="hybrid") 
+    GARCH.forecast <- ugarchforecast(GARCH.fit,data=data.set,n.ahead=22)
+    GARCH.est[(i-n.GARCH.start)+1] <- sqrt(mean(sigma(GARCH.forecast)^2))*sqrt(252)
+}
+  
+  GARCH.est
+}
+
+# Rolling estimates for GARCH(1,1) using 5 years of returns data, forecasts one month forward
+SPX.GARCH.norm.vol <- Roll.GARCH.norm.est(SPX.data,n.GARCH.start,n.GARCH.end)
+SPX.results.dates <- as.Date(SPX.df.date[1259:5503], format="%m/%d/%Y")
+SPX.results.data.frame <- cbind(SPX.rolling.vol[1258:5502],SPX.GARCH.norm.vol/100)
+
+SPX.hist.mse <- mean((SPX.results.data.frame[,2] - SPX.results.data.frame[,1])^2,na.rm=TRUE)
+
+# Plot 
+par(mar=c(6, 12, 4, 12) + 0.1)
+plot(SPX.results.dates, SPX.results.data.frame[,1], axes=F, ylim=c(-.2,1.0),type="l",col="black", xlab="", ylab="")
+axis(2, ylim=c(-.2,1.0),col="black",lwd=2)
+mtext(2,text="Realized Volatility Level",line=2.0)
+
+par(new=T)
+plot(SPX.results.dates, SPX.results.data.frame[,2], axes=F, ylim=c(-.2,1.0), type="l",lty=2,lwd=2, xlab="", ylab="", col="red", xaxt="n")
+axis(2, ylim=c(-.2,1.0),lwd=2,line=3.5)
+mtext(2,text="Predicted Volatility Level",line=5.5)
+
+difference <- (SPX.results.data.frame[,2] - SPX.results.data.frame[,1])
+mse <- mean((SPX.results.data.frame[,2] - SPX.results.data.frame[,1])^2,na.rm=TRUE)
+
+par(new=T)
+plot(SPX.results.dates, difference, axes=F, ylim=c(-.2,1.0), type="l",lty=2,lwd=2, xlab="", ylab="", col="blue", xaxt="n")
+abline(h=mse,col='grey50',lty=2,lwd=2)
+axis(4, ylim=c(-.2,1.0),lwd=2,line=3.5)
+mtext(4,text="SPX Realized - SPX Predicted",line=7.0)
+
+title(main="SPX Realized Volatility and  SPX Predicted Volatility")
+axis.Date(side=1,SPX.results.dates, at=seq(from=min(SPX.results.dates),to=max(SPX.results.dates),by=50), format="%m/%d/%Y")
+mtext("Date",side=1,col="black",line=2)
+
+legend("topleft",legend=c("SPX Realized Volatility Level","SPX Predicted Volatility", "SPX Realized - SPX Predicted", "MSE"),bty="n",lty=c(1,2,3,2), cex=0.60, col=c("black","red","blue","grey50")) 
+
